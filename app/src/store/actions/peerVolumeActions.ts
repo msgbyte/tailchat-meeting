695a2976e09375718e6177444823bdb6502fb3c7
chenruigang <chenruigang@clearofchina.com>
2022-07-08 10:18:16 +0800

    fix:修改点击音频设置白屏问题

--- app/src/store/actions/peerVolumeActions.ts
+++ app/src/store/actions/peerVolumeActions.ts
@@ -2,3 +2,7 @@ export const setPeerVolume = (peerId, volume) => ({
   type: 'SET_PEER_VOLUME',
   payload: { peerId, volume },
 });
+export const initPeerVolume = (peerId) => ({
+  type: 'SET_ME',
+  payload: { peerId },
+});
