fa4592f16339e7b6ece181516afa8557c761491d
chenruigang <chenruigang@clearofchina.com>
2022-07-08 10:18:16 +0800

    fix:修改点击音频设置白屏问题

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -28,6 +28,7 @@ import { detectDevice } from 'mediasoup-client';
 import { recorder } from './features/BrowserRecorder';
 import { config, configError } from './config';
 import { meActions } from './store/slices/me';
+import { initPeerVolume } from './store/actions/peerVolumeActions';
 
 import './index.css';
 
@@ -179,6 +180,8 @@ function run() {
       loginEnabled: config.loginEnabled,
     })
   );
+  // 初始化当前音量配置
+  store.dispatch(initPeerVolume(peerId));
   if (avatarUrl) {
     store.dispatch(meActions.setPicture(avatarUrl));
   }
