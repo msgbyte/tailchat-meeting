e73256b216e072a11fe7451af6762d947901df57
moonrailgun <moonrailgun@gmail.com>
2022-09-30 15:31:46 +0800

    fix: 修复vite环境下的编译Buffer问题

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
-
-// yarn add --dev @esbuild-plugins/node-globals-polyfill
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-// yarn add --dev @esbuild-plugins/node-modules-polyfill
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -46,6 +44,7 @@ export default defineConfig({
       zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
       tty: 'rollup-plugin-node-polyfills/polyfills/tty',
       domain: 'rollup-plugin-node-polyfills/polyfills/domain',
+      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
     },
   },
   optimizeDeps: {
@@ -67,9 +66,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       plugins: [
+        inject({ Buffer: ['buffer', 'Buffer'] }),
         // Enable rollup polyfills plugin
         // used during production bundling
-        rollupNodePolyFill(),
+        rollupNodePolyFill() as any,
       ],
     },
   },
